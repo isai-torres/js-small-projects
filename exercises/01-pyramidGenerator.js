7ad7fad881642f9be22abf8dd6e43d80847c82fb
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-11-26 19:47:40 -0400

    added exercise 01-pyramidGenerator

---
+++ exercises/01-pyramidGenerator.js
@@ -0,0 +1,16 @@
+/*Build a Pyramid Generator*/
+
+const character = "#"
+const count = 8;
+const rows = [];
+let result = "";
+
+for (let i = 0; i < count; i++) {
+  rows.push(character.repeat(i + 1));
+}
+
+for(const row of rows) {
+result = result + row + "\n";
+}
+
+console.log(result);
